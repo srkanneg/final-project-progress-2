6107d8703abb564f554059678975fd204815581a
jbened01 <jbened01@calpoly.edu>
2025-06-02 14:33:53 -0700

    Update classes.py

--- classes.py
+++ classes.py
@@ -27,68 +27,45 @@ class Task:
 
 
 class TodoList:
-    def __init__(self, task: Task):
-        self.task = task
+    def __init__(self):
+        self.tasks = []
 
     def __str__(self):
-        return "Task: {}".format(self.task)
+        result = ""
+        for task in self.tasks:
+            result += str(task) + "\n"
+        return result
 
     def __repr__(self):
-        return "To-do list {}".format(self.task)
+        return "To-do list {}".format(len(self.tasks))
 
-    def add_task(self):
-        tasks.append(self.task)
+    def add_task(self, task: Task):
+        self.tasks.append(task)
 
-    def remove_task(self):
-        updated_tasks = []
-        if self.task in tasks:
-            for i in tasks:
-                if self.task not in i:
-                    updated_tasks.append(i)
+    def remove_task(self, task: Task):
+        if task in self.tasks:
+            self.tasks.remove(task)
 
-    def view_tasks(self)->list[Task]:
-        for i in range(1, len(tasks)):
-            temp = tasks[i]
+    def view_tasks(self) -> list[Task]:
+        sorted_tasks = self.tasks.copy()
+        for i in range(1, len(sorted_tasks)):
+            temp = sorted_tasks[i]
             j = i - 1
-            while j >= 0 and temp.due_date < tasks[j].due_date:
-                tasks[j + 1] = tasks[j]
+            while j >= 0 and temp.due_date < sorted_tasks[j].due_date:
+                sorted_tasks[j + 1] = sorted_tasks[j]
                 j -= 1
-            tasks[j + 1] = temp
-        return tasks
+            sorted_tasks[j + 1] = temp
+        return sorted_tasks
 
-    def view_completed_tasks(self, tasks:list[Task])->list[Task]:
-        updated_tasks = []
-        for i in range(1, len(tasks)):
-            temp = tasks[i]
-            j = i - 1
-            while j >= 0 and temp.due_date < tasks[j].due_date:
-                tasks[j + 1] = tasks[j]
-                j -= 1
-            tasks[j + 1] = temp
-        for j in tasks:
-            if tasks[j].completed:
-                updated_tasks.append(tasks[j])
-        return updated_tasks
+    def view_completed_tasks(self) -> list[Task]:
+        sorted_tasks = self.view_tasks()
+        return [task for task in sorted_tasks if task.completed]
 
-    def view_incomplete_tasks(self, tasks:list[Task])->list[Task]:
-        updated_tasks = []
-        for i in range(1, len(tasks)):
-            temp = tasks[i]
-            j = i - 1
-            while j >= 0 and temp.due_date < tasks[j].due_date:
-                tasks[j + 1] = tasks[j]
-                j -= 1
-            tasks[j + 1] = temp
-        for j in tasks:
-            if tasks[j].completed == False:
-                updated_tasks.append(tasks[j])
-        return updated_tasks
+    def view_incomplete_tasks(self) -> list[Task]:
+        sorted_tasks = self.view_tasks()
+        return [task for task in sorted_tasks if not task.completed]
 
-    def view_category(self, tasks:list[Task], category:str)->list[Task]:
-        updated_tasks = []
-        for task in tasks:
-            if task.category == category:
-                updated_tasks.append(task)
-        return updated_tasks
+    def view_category(self, category: str) -> list[Task]:
+        return [task for task in self.tasks if task.category == category]
 
 

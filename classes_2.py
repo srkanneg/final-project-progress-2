d54dd1ac66ca27bdda6b74df61fc3b49d87e2832
jbened01 <jbened01@calpoly.edu>
2025-06-01 21:12:09 -0700

    jack

---
+++ classes_2.py
@@ -0,0 +1,69 @@
+class Task:
+    def __init__(self, name, description, due_date, category, time, completed, location) -> None:
+        self.name = name
+        self.description = description
+        self.due_date = due_date
+        self.category = category
+        self.time = time
+        self.completed = completed
+        self.location = location
+
+    def __str__(self) -> str:
+        return "name: {}, description: {}, due_date: {}, category: {}, time: {}, completed: {}, location: {}".format(
+            self.name, self.description, self.due_date, self.category, self.time, self.completed, self.location)
+
+    def __repr__(self) -> str:
+        return "Task {}, {}, {}, {}, {}, {}, {}".format(self.name, self.description, self.due_date, self.category,
+                                                        self.time, self.completed, self.location)
+
+    def mark_as_complete(self):
+        self.completed = True
+
+    def get_status(self):
+        if self.completed:
+            print("Status: Completed")
+        else:
+            print("Status: In progress")
+
+
+class TodoList:
+    def __init__(self):
+        self.tasks = []
+
+    def __str__(self):
+        result = ""
+        for task in self.tasks:
+            result += str(task) + "\n"
+        return result
+
+    def __repr__(self):
+        return "To-do list {}".format(len(self.tasks))
+
+    def add_task(self, task: Task):
+        self.tasks.append(task)
+
+    def remove_task(self, task: Task):
+        if task in self.tasks:
+            self.tasks.remove(task)
+
+    def view_tasks(self) -> list[Task]:
+        sorted_tasks = self.tasks.copy()
+        for i in range(1, len(sorted_tasks)):
+            temp = sorted_tasks[i]
+            j = i - 1
+            while j >= 0 and temp.due_date < sorted_tasks[j].due_date:
+                sorted_tasks[j + 1] = sorted_tasks[j]
+                j -= 1
+            sorted_tasks[j + 1] = temp
+        return sorted_tasks
+
+    def view_completed_tasks(self) -> list[Task]:
+        sorted_tasks = self.view_tasks()
+        return [task for task in sorted_tasks if task.completed]
+
+    def view_incomplete_tasks(self) -> list[Task]:
+        sorted_tasks = self.view_tasks()
+        return [task for task in sorted_tasks if not task.completed]
+
+    def view_category(self, category: str) -> list[Task]:
+        return [task for task in self.tasks if task.category == category]
